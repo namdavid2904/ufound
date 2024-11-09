cd38ee544e2db5a6b87bd78920831efd5063716f
namvinhkhang <83082969+namvinhkhang@users.noreply.github.com>
2024-11-09 14:23:11 -0500

    New Version
    Support Crop image to zoom and Rotate to the upright position for better text recognition

--- Text_detection.py
+++ Text_detection.py
@@ -1,34 +1,16 @@
 import pytesseract
-import cv2
-from ultralytics import YOLO
+from Image_processing import ImageProccessing
+from flask import jsonify
 
-crop_model = YOLO("yolov8s.pt")
-
-def crop_img(img_link):
-    img = cv2.imread(img_link)
-    result = crop_model.predict(img_link)
-
-    for r in result:
-        cropped_image = img
-        for box in r.boxes:
-            if int(box.cls) != 73:
-                continue
-            x_min, y_min, x_max, y_max = map(int, box.xyxy[0])
-            cropped_image = img[int(y_min) : int(y_max), int(x_min) : int(x_max)]
-        cv2.imwrite(f'modified_input_img/cropped_id_card.jpg', cropped_image)
 
 #All the text appear in the image
-text = pytesseract.image_to_string(f'modified_input_img/cropped_id_card1.jpg')
 
 def extract_info(text):
     name = ''
     spire_id = ''
     line_of_string = text.split('\n')
-    print(line_of_string)
     for s in line_of_string:
-        print(s)
         words = s.split(" ")
-        print(words)
         for w in words:
             all_num = True
             for c in w:
@@ -39,6 +21,12 @@ def extract_info(text):
                 spire_id = w
         if (spire_id != ''):
             name = s[9:]
-            return (name, spire_id)
-            
+            return {'name': name,
+                    'id': spire_id}
+    
+img_proccess = ImageProccessing("models/yolov8m-seg.pt")
+filepath = 'input_img/test (3).jpeg' #Specified the file path to the original img
+new_img = img_proccess.process(filepath)
+
+text = pytesseract.image_to_string(new_img)
 print(extract_info(text))
