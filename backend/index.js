c5560a649434865180a91401d5b893f3c3caa890
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2024-11-09 11:45:43 -0500

    Updated set up

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
+const dbConnect = require('./config/dbConnect');
+
+dbConnect();
+app.use(morgan());
+app.use(cors());
+app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
