099c2352f11ca119fdfacc4206df87c5591c61a3
mnhng1 <minhnguyentuong1@gmail.com>
2024-11-09 03:44:54 -0500

    Create a form template for general item, and refactor the code.

--- frontend/src/components/ReportForm.jsx
+++ frontend/src/components/ReportForm.jsx
@@ -4,30 +4,105 @@ import { Camera, MapPin, CreditCard} from 'lucide-react';
 
 function ReportForm() {
     const [itemType, setItemType] = useState('ucard')
-    const [formData,setFormData] = useState({
+
+    //For UCard
+    const [cardFormData,setCardFormData] = useState({
         studentName: '',
         cardNumber: '',
-        location: ''
+        location: '',
+        pinLocation: ''
     })
 
+    //For General Item
+    const [generalFormData,setGeneralFormData] = useState({
+        itemName: '',
+        itemDescription: '',
+        location: '',
+        pinLocation: ''
+    })
+
+
     const handleInputChange = (e) => {
-        setFormData({
-          ...formData,
+        setCardFormData({
+          ...cardFormData,
           [e.target.name]: e.target.value
         });
       };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(formData);
+        console.log(cardFormData);
+    }
+    //Helper method to check required fields 
+    const isFormValid = () => {
+        return cardFormData.studentName && cardFormData.cardNumber && cardFormData.location;
+    };
+    
+
+    //Template for UCard
+    const TemplateUCard = () => {
+        return (
+        <div className='flex flex-col  gap-3'>
+            <button
+                    type="button"
+                    className='w-full bg-[#212721] text-white rounded-xl py-3 px-4 flex justify-center items-center space-x-2 hover:bg-gray-800 transition-colors'
+                >    <Camera className="w-5 h-5 mr-3" />
+                    <span>Take a Picture of UCard</span>
+                </button>
+                <div className = "space-y-2">
+                <label className='block text-base font-semibold'> Student Name</label>
+                <input
+                    type="text"
+                    name="studentName"
+                    className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent'
+                    placeholder='E.g. John Doe'
+                    value={cardFormData.studentName}
+                    onChange = {handleInputChange}
+                />
+                </div>
+                <div className = "space-y-2">
+                    <label className='block text-base font-semibold'> Spire ID</label>
+                    <input
+                        type="text"
+                        name="cardNumber"
+                        className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent'
+                        placeholder='E.g. 12345678'
+                        value = {cardFormData.cardNumber}
+                        onChange = {handleInputChange}
+                    />
+                </div>
+                <div className = "space-y-2">
+                    <label className='block text-base font-semibold'>Found Location</label>
+                    <input
+                        type="text"
+                        name="location"
+                        className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent'
+                        placeholder='E.g. Franklin 2nd floor, across Deli'
+                        value={cardFormData.location}
+                        maxLength={8}
+                        onChange = {handleInputChange}
+                    />
+                </div>
+        </div>
+        )
     }
 
-    const isFormValid = () => {
-        return formData.studentName && formData.cardNumber && formData.location;
-    };
 
+    //Template for General Item
+    const TemplateGeneralItem = () => {
+        return (
+            <div>Currently Working...</div>
+        )
+    }
+
+
+    //Default render template for the Form
     return (
-        <form className='flex flex-col  gap-3'>
+        <div>
+        
+        <form className='flex flex-col  gap-3 border border-color-gray px-4 py-4 rounded-lg'>
+            <h1 className='text-2xl font-semibold'>Report Found Item</h1>
+            <h2 className='text-gray-400'>Provide details about the item you found</h2>
             <label className = 'flex items-center space-x-3'>
             <div>
                 <input
@@ -46,69 +121,7 @@ function ReportForm() {
             </div>
             <span className="text-base">UCard</span>
             </label>
-            
-            {itemType === 'ucard' && (
-                <button
-                    type="button"
-                    className='w-full bg-[#212721] text-white rounded-xl py-3 px-4 flex justify-center items-center space-x-2 hover:bg-gray-800 transition-colors'
-                >    <Camera className="w-5 h-5 mr-3" />
-                    <span>Take a Picture of UCard</span>
-                </button>
-            )}
-
-            <div className = "space-y-2">
-                <label className='block text-base font-semibold'> Student Name</label>
-                <input
-                    type="text"
-                    name="studentName"
-                    className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent'
-                    placeholder='E.g. John Doe'
-                    value={formData.studentName}
-                    onChange = {handleInputChange}
-                />
-            </div>
-            <div className = "space-y-2">
-                <label className='block text-base font-semibold'> Spire ID</label>
-                <input
-                    type="text"
-                    name="cardNumber"
-                    className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent'
-                    placeholder='E.g. 12345678'
-                    value = {formData.cardNumber}
-                    onChange = {handleInputChange}
-                />
-            </div>
-            <div className = "space-y-2">
-                <label className='block text-base font-semibold'>Found Location</label>
-                <input
-                    type="text"
-                    name="location"
-                    className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent'
-                    placeholder='E.g. Franklin 2nd floor, across Deli'
-                    value={formData.location}
-                    maxLength={8}
-                    onChange = {handleInputChange}
-                />
-            </div>
-
-            <button type = 'submit' className='w-full flex items-center justify-center bg-[#212721] text-white rounded-xl py-3 px-4 space-x-2 hover:bg-gray-500 transition-colors' >
-                <MapPin className="w-5 h-5" />
-                <span>Map AR Pin to Location</span>
-            </button>
-
-            <button
-                type="submit"
-                className={`w-full flex items-center justify-center ${isFormValid() ? 'bg-gray-400 text-white' : 'bg-gray-300 text-gray-500 cursor-not-allowed'} rounded-xl py-3 px-4 space-x-2 hover:bg-gray-500 transition-colors`}
-                onClick={handleSubmit}
-                disabled = {!isFormValid()}
-            >
-                <CreditCard className="w-5 h-5" />
-                <span>Report Found Item</span>
-            </button>
-        </form>
-        /* Neu lam general item thi them cai nay vo cai form o tren
-
-        <label className = 'flex items-center space-x-3'>
+            <label className = 'flex items-center space-x-3'>
             <div>
                 <input
                     type="radio"
@@ -126,9 +139,37 @@ function ReportForm() {
             </div>
             <span className="text-base">General Items</span>
             </label>
+            {itemType === 'ucard' && (
+                <TemplateUCard/>
+            )}
+
+            {itemType === 'general' && (
+                <TemplateGeneralItem/>
+            )}
+
+
+            <button type = 'submit' className='w-full flex items-center justify-center bg-[#212721] text-white rounded-xl py-3 px-4 space-x-2 hover:bg-gray-500 transition-colors' >
+                <MapPin className="w-5 h-5" />
+                <span>Map AR Pin to Location</span>
+            </button>
+
+            <button
+                type="submit"
+                className={`w-full flex items-center justify-center ${isFormValid() ? 'bg-[#962a3f] text-white' : 'bg-gray-300 text-gray-500 cursor-not-allowed'} rounded-xl py-3 px-4 space-x-2 hover:bg-gray-500 transition-colors disabled:bg-gray-300`}
+                onClick={handleSubmit}
+                disabled = {!isFormValid()}
+            >
+                <CreditCard className="w-5 h-5" />
+                <span>Report Found Item</span>
+            </button>
+        </form>
+        </div>
+        /* Neu lam general item thi them cai nay vo cai form o tren
+
+        
         */
     )
-
 }
+
 
 export default ReportForm;
