88e80fd2f1ad80932fd74e1c0b096c781a3953c3
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2024-11-10 02:00:42 -0500

    Create TTL index to auto delete cards after 1 week created

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+const Card = require('../models/Card');
 
 const dbConnect = async() => {
     try {
         const connection = await mongoose.connect(process.env.MONGODB_URL);
         console.log('DB connected');
+
+        // Create Time-To-Live index to auto delete cards after 1 week
+        Card.collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 604800 });        
     } catch(error) {
         console.error("Error connecting to database", error);
     }
