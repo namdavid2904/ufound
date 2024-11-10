f9e26156bcb25a2f28adaae44f6cb46096c065b1
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2024-11-10 04:16:41 -0500

    Complete set up routes

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,18 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const dbConnect = require('./config/dbConnect');
+const userRouter = require('./routes/userRoutes');
+const cardRouter = require('./routes/cardRoutes');
+
 
 dbConnect();
 app.use(morgan());
 app.use(cors());
 app.use(express.json());
+app.use('api/user', userRouter);
+app.use('api/card', cardRouter);
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
